574929b733f891af66ef40230c3d95b6794ab6d1
PierrunoYT <pierrebruno@hotmail.ch>
2025-10-04 10:50:53 +0200

    chore(release): bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="deepseek-cli",
-    version="0.1.24",
+    version="0.2.0",
     author="PierrunoYT",
     author_email="pierrebruno@hotmail.ch",
     description="A powerful CLI for interacting with DeepSeek's AI models",
--- src/__init__.py
+++ src/__init__.py
@@ -6,4 +6,4 @@ from . import config
 from . import handlers
 from . import utils
 
-__version__ = "0.1.24"
+__version__ = "0.2.0"
